4f399532cff68b1c7562a4ec87c3f299a77f8785
RomuloCVieira <romuloevil@outlook.com>
2024-08-28 19:13:13 -0300

    build: nova feature

---
+++ public/build/assets/TextInput-B_Drq-2N.js
@@ -0,0 +1,1 @@
+import{r,j as f}from"./app-5lAZS7Xu.js";const c=r.forwardRef(function({type:e="text",className:o="",isFocused:n=!1,...u},t){const s=t||r.useRef();return r.useEffect(()=>{n&&s.current.focus()},[]),f.jsx("input",{...u,type:e,className:"rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "+o,ref:s})});export{c as T};
